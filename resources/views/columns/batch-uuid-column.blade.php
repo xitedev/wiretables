cdea0f9d64b19977ada0821704de08f3429a30a9
Oleksandr Petrovskyi <xitedev@gmail.com>
2023-03-02 14:23:35 +0200

    fix icons

--- resources/views/columns/batch-uuid-column.blade.php
+++ resources/views/columns/batch-uuid-column.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-row space-x-1 items-center">
     @if($filter && $filterValue)
         <span class="cursor-pointer" @click.prevent="$wire.addFilterOutside('{{ $filter }}', '{{ $filterValue }}')">
-            @svg($icon ?? 'heroicon-o-filter', 'flex-shrink-0 h-4 w-4 text-gray-300 transition ease-in-out duration-150')
+            @svg($icon ?? 'heroicon-o-funnel', 'flex-shrink-0 h-4 w-4 text-gray-300 transition ease-in-out duration-150')
         </span>
     @endif
 
--- resources/views/columns/button-column.blade.php
+++ resources/views/columns/button-column.blade.php
@@ -3,5 +3,5 @@
     wire:click="$emit('openModal', '{{ $modal }}', {'model': {{ $id }}})"
     class="text-gray-500 flex items-center space-x-1 justify-center w-16 mx-auto"
     :outline="$outline"
-    icon="heroicon-o-photograph"
+    icon="heroicon-o-photo"
 />
